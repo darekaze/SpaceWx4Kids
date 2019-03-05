64a2f642f956e0c6110b33efe086940f5fbd5b2c
darekaze <darekaze@gmail.com>
2019-03-05 11:40:32 +0800

    Add game temporary template

--- src/views/Games.vue
+++ src/views/Games.vue
@@ -1,13 +1,27 @@
 <template>
-  <div></div>
+  <sub-page :title="$t('games')" icon="icons/ph_icon.svg">
+    <v-container grid-list-md fill-height pt-3>
+      <div class="white--text display-1">Under Development, Coming Soon...</div>
+    </v-container>
+  </sub-page>
 </template>
 
 <script>
 export default {
-
+  name: 'games-page',
+  components: {
+    SubPage: () => import('@/components/SubPageTemplate.vue'),
+  },
 };
 </script>
 
-<style>
-
-</style>
+<i18n>
+{
+  "en": {
+    "games": "Minigames"
+  },
+  "zh": {
+    "games": "小遊戲"
+  }
+}
+</i18n>
