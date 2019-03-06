f40f4c657e734ba8509cab796742fba421b6abb0
darekaze <darekaze@gmail.com>
2019-03-06 17:59:45 +0800

    Adjust homepage background

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,15 +1,40 @@
 <template>
-  <div class="dark space-bg">
+  <div class="dark " :class="bg">
     <slot/>
   </div>
 </template>
 
+<script>
+export default {
+  computed: {
+    atHome() {
+      return /^\/\w+\/?$/.test(this.$route.path); // match locale route
+    },
+    bg() {
+      // Switch bg-img at HomeDesktop, otherwise don't
+      const homeAndDesktop = this.atHome && this.$vuetify.breakpoint.mdAndUp;
+      return `space-bg-${homeAndDesktop ? 'special' : 'normal'}`;
+    },
+  },
+};
+</script>
+
 <style lang="scss" scoped>
 .space-bg {
   background-color: #263238;
-  background-image: url('../assets/images/space_background.jpg');
-  background-repeat: repeat-y;
-  background-size: 100vw auto;
-  min-height: calc(100vh - 124px);
+
+  &-normal {
+    background-image: url('../assets/images/space_background.jpg');
+    background-repeat: repeat-y;
+    background-size: 100vw auto;
+    min-height: calc(100vh - 124px);
+  }
+
+  &-special { // Need further change
+    background-image: url('../assets/images/index.png');
+    background-repeat: repeat-y;
+    background-size: 100vw auto;
+    min-height: calc(100vh - 124px);
+  }
 }
 </style>
