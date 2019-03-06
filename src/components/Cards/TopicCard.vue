681184a000cc0108abb7382fd43aa91460194180
darekaze <darekaze@gmail.com>
2019-03-06 11:16:04 +0800

    Add sun and earth and adjust homepage

--- src/components/Cards/TopicCard.vue
+++ src/components/Cards/TopicCard.vue
@@ -1,20 +1,22 @@
 <template>
   <v-card
     dark flat color="transparent"
-    class="pb-4"
+    :class="`pb-${cardGap}`"
     :to="$i18nRoute({ name: to })">
     <v-img contain
       :src="require(`@/assets/icons/${img}.png`)"
       transition="scale-transition"
       max-height="270"/>
     <v-card-text
-      class="py-3 text-xs-center grey--text text--lighten-3"
+      class="text-xs-center grey--text text--lighten-3"
       :class="{
         headline: $vuetify.breakpoint.smAndUp,
         subheading: $vuetify.breakpoint.xsOnly,
+        'py-2': this.mobile,
+        'py-3': !this.mobile,
       }">
-      <div>
-        <span class="tint-black px-2">{{ title }}</span>
+      <div class="text-xs-center">
+        <span class="tint-black">{{ title }}</span>
       </div>
     </v-card-text>
   </v-card>
@@ -26,6 +28,14 @@ export default {
     title: String,
     img: String,
     to: String,
+  },
+  computed: {
+    mobile() {
+      return this.$vuetify.breakpoint.xsOnly;
+    },
+    cardGap() {
+      return this.mobile ? 2 : 4;
+    },
   },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,12 @@
 <template>
   <div id="home">
     <v-container pb-0>
-      <v-layout row justify-space-between>
-        <v-flex xs12 sm6>
+      <v-layout row justify-start>
+        <v-flex xs12 sm7>
           <v-img contain
             :src="require(`@/assets/icons/${$t('logo')}.png`)"
             transition="scale-transition"
             max-width="460"/>
-        </v-flex>
-        <v-flex v-if="!mobile">
-          <!-- desktop sun here -->
         </v-flex>
       </v-layout>
     </v-container>
@@ -29,6 +26,16 @@
         </v-layout>
       </v-layout>
     </v-container>
+    <v-container pa-0 fill-height>
+      <v-layout row justify-space-around>
+        <v-flex xs5>
+          <topic-card :title="$t('earth')" img="nh_earth" to="earth"/>
+        </v-flex>
+        <v-flex xs3>
+          <topic-card :title="$t('sun')" img="nh_sun" to="sun"/>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
@@ -36,11 +43,6 @@
 export default {
   components: {
     TopicCard: () => import('@/components/Cards/TopicCard.vue'),
-  },
-  computed: {
-    mobile() {
-      return this.$vuetify.breakpoint.xsOnly;
-    },
   },
 };
 </script>
@@ -52,14 +54,18 @@ export default {
     "phenomena": "Phenomena",
     "impacts": "Impacts",
     "historical-events": "Historical Events",
-    "alerts": "Alerts"
+    "alerts": "Alerts",
+    "sun": "Sun",
+    "earth": "Earth"
   },
   "zh": {
     "logo": "logo_zh",
     "phenomena": "現象",
     "impacts": "影響",
     "historical-events": "有趣的歷史事件",
-    "alerts": "警報"
+    "alerts": "警報",
+    "sun": "太陽",
+    "earth": "地球"
   }
 }
 </i18n>
