e09168307282557eb481447bad146efaa8d12dee
darekaze <darekaze@gmail.com>
2019-03-20 12:16:13 +0800

    Modify game links and plan for further change

--- src/views/Games.vue
+++ src/views/Games.vue
@@ -1,16 +1,15 @@
 <template>
   <sub-page :title="$t('games')" icon="icons/game_icon.png">
     <v-container grid-list-md pt-3 px-5>
-      <v-layout row wrap justify-center v-if="$vuetify.breakpoint.smAndUp">
+      <v-layout row wrap justify-center>
         <v-flex xs12 sm6
           class="pa-2"
           v-for="value in gamesInfo" :key="value.link">
           <game-card :info="value"/>
         </v-flex>
-      </v-layout>
-      <v-layout row wrap justify-center v-else>
         <v-flex xs12>
-          Sorry
+          <!-- TODO: Add webgl tutorial -->
+          <!-- Card here -->
         </v-flex>
       </v-layout>
     </v-container>
@@ -18,7 +17,7 @@
 </template>
 
 <script>
-import gamesInfo from '@/data/games.json';
+import gamesInfo from '@/data/game/games.json';
 
 export default {
   name: 'games-page',
